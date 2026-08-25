fba2bd9162cfedf8ed1b827ace67ac9617d54b7b
Sujal Thaware <143960930+sujalthaware@users.noreply.github.com>
2026-08-25 15:31:55 +0530

    first program tp

---
+++ firstprogram.py
@@ -0,0 +1,90 @@
+###Python Full Course❤️ | Variables & Data Types | Lecture 1
+"""
+Introduction to Python
+08:01 - Python Installation (Windows)
+09:26 - Python Installation (Mac)
+10:26 - VSCode Installation
+12:38 - First Program
+17:10 - Python Character Set
+20:50 - Variables and how to use them
+30:02 - Rules for Identifiers
+33:25 - Data Types
+39:56 - Keywords
+42:51 - Print Sum
+45:12 - Comments in Python
+47:13 - Operators in Python
+1:02:24 - Type Conversion
+1:08:41 - Inputs in Python
+1:15:52 - Let's Practice
+"""
+
+print("hello world")
+a=2
+b=3
+print(a*b)
+
+# name = input("Enter your name: ")
+# print("Hello " + name)
+
+name = "sujal" 
+age= 20
+prirce= 1000.50
+
+print(type(name))
+print(type(age))
+print(type(prirce))
+
+jevla_ka = True
+print(type(jevla_ka))
+
+nahi_jevala_ka = False
+print(type(nahi_jevala_ka))
+
+nko_jevu = None
+print(type(nko_jevu))
+
+a= 10
+b= 20
+c= 30
+sum = a+b+c
+print(sum)
+
+##arithmetic operators
+a= 10
+b= 20
+sun = a+b
+sub = a-b
+mul = a*b
+div = a/b
+mod = a%b
+print("sum is: ", sun)
+print("difference is: ", sub)
+print("product is: ", mul)
+print("quotient is: ", div)
+print("remainder is: ", mod) 
+
+#relational operators
+a= 10
+b= 20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#assignment operators
+a= 10
+a += 5  # a = a + 5
+print(a)
+
+#logical operators
+a= 10
+b= 20
+print(a>15 and b>15)
+
+print(a>1 or b>100)
+
+print(not(a<5))
+
+
