ec9973a2e8ef8d3fe4f2a8482ceb04afef8e00b4
Sujal Thaware <143960930+sujalthaware@users.noreply.github.com>
2026-08-25 15:35:00 +0530

    variables

---
+++ # Creating variables.py
@@ -0,0 +1,13 @@
+# Creating variables
+
+name = "Sujal"
+age = 24
+temperature = 32.5
+is_student = True
+
+# Printing variables
+
+print("Name:", name)
+print("Age:", age)
+print("Temperature:", temperature)
+print("Is Student:", is_student)
